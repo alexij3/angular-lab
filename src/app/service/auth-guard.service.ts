6c904c371ff46d6824c7c1d78fce7e91f95bac38
Olexij <alexbuzilov.official@gmail.com>
2019-09-30 08:02:38 +0300

    -- Added Auth Guard.

---
+++ src/app/service/auth-guard.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {AuthenticationService} from './authentication.service';
+import {Observable} from 'rxjs';
+import {decode} from 'punycode';
+import {AppComponent} from '../app.component';
+import {TokenStorageService} from './token-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private authService: AuthenticationService,
+              private router: Router,
+              private tokenService: TokenStorageService) { }
+
+  canActivate(route: ActivatedRouteSnapshot): boolean{
+    const expectedRole = route.data.expectedRole;
+
+    const hasRole = this.tokenService.getAuthorities().indexOf(expectedRole) >= 0;
+    if (!this.authService.isAuthenticated() || !hasRole) {
+      return false;
+    }
+    return true;
+  }
+
+}
